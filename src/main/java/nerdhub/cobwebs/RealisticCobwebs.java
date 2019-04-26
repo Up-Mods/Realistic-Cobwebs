9efc586d57e782cfe0d472f565e1c843a2bfd154
Raph Hennessy <raph.hennessy@gmail.com>
2019-04-26 21:37:23 +1000

    update to 1.14

--- src/main/java/nerdhub/cobwebs/RealisticCobwebs.java
+++ src/main/java/nerdhub/cobwebs/RealisticCobwebs.java
@@ -6,7 +6,7 @@ import nerdhub.cobwebs.util.ConfigReloader;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.WebBlock;
+import net.minecraft.block.CobwebBlock;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -32,14 +32,14 @@ public class RealisticCobwebs implements ModInitializer {
             Item item = stack.getItem();
             if(item == Item.getItemFromBlock(Blocks.TORCH)|| item == Items.FLINT_AND_STEEL){
                 if(blockHitResult.getType() == HitResult.Type.BLOCK){
-                    if(world.getBlockState(blockHitResult.getBlockPos()).getBlock() instanceof WebBlock){
+                    if(world.getBlockState(blockHitResult.getBlockPos()).getBlock() instanceof CobwebBlock){
                         if(world instanceof ServerWorld){
                             BlockPos webPos = blockHitResult.getBlockPos();
                             ServerWorld server = (ServerWorld) world;
-                            server.method_14199(ParticleTypes.FLAME, webPos.getX() + 0.5D, webPos.getY() + 0.5D, webPos.getZ() + 0.5D, 7 + random.nextInt(40), random.nextDouble() * 0.5D, random.nextDouble() * 0.5D, random.nextDouble() * 0.5D,0.005);
-                            world.clearBlockState(webPos);
+                            server.spawnParticles(ParticleTypes.FLAME, webPos.getX() + 0.5D, webPos.getY() + 0.5D, webPos.getZ() + 0.5D, 7 + random.nextInt(40), random.nextDouble() * 0.5D, random.nextDouble() * 0.5D, random.nextDouble() * 0.5D,0.005);
+                            world.clearBlockState(webPos, false);
                             if(item == Items.FLINT_AND_STEEL){
-                                stack.applyDamage(1,playerEntity);
+                                stack.applyDamage(1, playerEntity, (player) -> {  });
                             }else if(item == Items.TORCH && random.nextDouble()< ConfigHandler.getConfig(ModConfig.class).torchConsumeChance){
                                 stack.subtractAmount(ConfigHandler.getConfig(ModConfig.class).flintAndSteelDamagePerUse);
                             }
